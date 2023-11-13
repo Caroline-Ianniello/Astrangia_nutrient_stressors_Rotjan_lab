d0885a45de022caf254d034b501eb6002943ef44
ChenTaHung <denny20700@gmail.com>
2023-11-13 11:50:28 -0500

    add: random k-fold parameter of numbers of observatino in each fold

--- MSSP/src/modeling/k-fold-CV.R
+++ MSSP/src/modeling/k-fold-CV.R
@@ -6,19 +6,32 @@ if(!require('caret', character.only = T)){
 
 library(caret)
 
-k_fold_CV <- function(data, k, random_k_fold = F){
+k_fold_CV <- function(data, k, random_k_fold = F, rand_select_nobs = NULL){
   
   # K-Fold Cross Validation
   # ======
   # data : data to split
   # k : number of folds
   # random_k_fold: <bool> specify whether sample by random for each folds (duplicated data might exist)
+  # rand_select_nobs: <float, int> number or proportion to sample in each fold.
   # ======
   
   nobs <- nrow(data)
   
   if (random_k_fold){ # randomized sample for each fold, replace = TRUE
-    id_sample_mat <- replicate(n = k, sample(c(1:nobs), size = round(nobs/k, 0), replace = TRUE), simplify = 'list')
+    if(is.null(rand_select_nobs)){ # by default
+      id_sample_mat <- replicate(n = k, sample(c(1:nobs), size = round(nobs/k, 0), replace = TRUE), simplify = 'list')  
+    }
+    else{
+      if(rand_select_nobs > 0 & rand_select_nobs <= 1){
+        # select by proportion
+        id_sample_mat <- replicate(n = k, sample(c(1:nobs), size = round(nobs*rand_select_nobs, 0), replace = TRUE), simplify = 'list')  
+      }
+      else{
+        # select by number
+        id_sample_mat <- replicate(n = k, sample(c(1:nobs), size = round(rand_select_nobs, 0), replace = TRUE), simplify = 'list')  
+      }
+    }
     k_fold_id <- list()
     for(i in 1:ncol(id_sample_mat)){
       k_fold_id[[paste0('fold', i)]] <- id_sample_mat[,i]
