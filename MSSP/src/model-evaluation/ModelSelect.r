7a1289799d5836b709ace5ad8c2e42154fc868b9
chenzhi123 <1049569420@qq.com>
2023-11-14 16:33:34 -0500

    add output_model_details and save_to_markdown function

--- MSSP/src/model-evaluation/ModelSelect.r
+++ MSSP/src/model-evaluation/ModelSelect.r
@@ -46,3 +46,72 @@ find_best_lambda <- function(performance_metrics) {
 
 #find_best_lambda(performance_metrics)
 
+
+
+output_model_details <- function(model_fit, best_lambda) {
+
+  for (i in 1:5) {
+    fold_data <- model_fit[[paste0("fold", i)]]
+    
+    for (j in 1:length(fold_data)) {
+      if (fold_data[[j]]$lambda == best_lambda) {
+      
+        selected_model <- fold_data[[j]]$fit
+        
+        
+        print(summary(selected_model))
+        
+        
+        coefficients <- coef(selected_model)
+        print(coefficients)
+        
+        return(list(summary = summary(selected_model), coefficients = coefficients))
+      }
+    }
+  }
+  
+
+  print("No model found with the specified lambda.")
+}
+
+# best_lambda <- find_best_lambda(performance_metrics)[[1]]
+# best_lambda
+
+# model_details <- output_model_details(nitrate_percent_change_fit, best_lambda)
+# model_details
+
+save_to_markdown <- function(model_fit, performance_metrics, file_path, file_name) {
+  
+  markdown_path <- paste0(file_path, "/", file_name, ".md")
+  
+  
+  best_lambda <- find_best_lambda(performance_metrics)
+  
+  
+  model_details <- output_model_details(model_fit, best_lambda$lambda)
+  
+  
+  file_conn <- file(markdown_path, "w")
+  
+  
+  writeLines(paste0("# Best Lambda\n\n", 
+                    "* Lambda: ", best_lambda$lambda, "\n", 
+                    "* Fold: ", best_lambda$fold, "\n\n"), file_conn)
+  
+  
+  writeLines("# Model Summary\n\n```\n", file_conn)
+  capture.output(print(model_details$summary), file = file_conn)
+  writeLines("```\n\n", file_conn)
+  
+  writeLines("# Model Coefficients\n\n```\n", file_conn)
+  capture.output(print(model_details$coefficients), file = file_conn)
+  writeLines("```\n\n", file_conn)
+  
+  
+  close(file_conn)
+}
+
+
+# file_path <- "D:/Project/Astrangia_nutrient_stressors_Rotjan_lab/MSSP/doc" 
+# file_name <- "Best_percent_lambda"           
+# save_to_markdown(nitrate_percent_change_fit, performance_metrics, file_path, file_name)
